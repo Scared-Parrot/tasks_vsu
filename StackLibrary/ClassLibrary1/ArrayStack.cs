c93b355d52ca3756cd649818569f2696b9672c1e
Сипин Илья <63446770+Scared-Parrot@users.noreply.github.com>
2020-06-26 00:35:43 +0300

    Add files via upload

---
+++ StackLibrary/ClassLibrary1/ArrayStack.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class ArrayStack<T>: IStack <T>
+    {
+        private T[] arrayData;
+        private const int defaultSize = 30;
+        private int index;
+
+        public int Count => index;
+
+        public bool IsEmpty => index == 0;
+
+        public ArrayStack()
+        {
+            arrayData = new T[defaultSize];
+            index = 0;
+        }
+
+        public ArrayStack(int size)
+        {
+            if (size < 0)
+                throw new StackException("Size must be a positive number");
+
+            arrayData = new T[size];
+            index = 0;
+        }
+        public T Pop()
+        {
+            if (index == 0)
+                throw new StackException("Exception: Empty stack");
+
+            return arrayData[--index];
+
+        }
+
+        public T Peek()
+        {
+            if (index == 0)
+                throw new StackException("Exception: Empty stack");
+
+            return arrayData[index - 1];
+
+        }
+
+        public void Push(T obj)
+        {
+            if (index == arrayData.Length)
+            {
+                T[] newArray = new T[2 * arrayData.Length];
+                Array.Copy(arrayData, 0, newArray, 0, index);
+                arrayData = newArray;
+            }
+            arrayData[index] = obj;
+            index++;
+        }
+
+        public void Clear()
+        {
+            arrayData = new T[defaultSize];
+            index = 0;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < index; i++)
+            {
+                yield return arrayData[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ StackLibrary/ClassLibrary1/IStack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public interface IStack <T>: IEnumerable <T>
+    {
+        void Push(T value);
+        void Clear();
+        T Pop();
+        T Peek();
+        int Count { get; } 
+        bool IsEmpty { get; }
+    }
+}
